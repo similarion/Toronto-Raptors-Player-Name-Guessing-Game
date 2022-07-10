c6da1060904f967fed56ce6fe98933f9582ba34b
Kawa Cheng <similarion@gmail.com>
2022-07-09 23:42:05 -0400

    commit

--- GUESSING GAME.py
+++ GUESSING GAME.py
@@ -1,4 +1,6 @@
-import random
+while True:
+
+  import random
 # library that we use in order to choose
 # on random words from a list of words
 
@@ -7,74 +9,83 @@ import random
 
 #print("Good Luck ! ", name)
 
-words = ['Christian Koloko', 'Armoni Brooks', 'OG Anunoby', 'Scottie Barnes',
+  words = ['Christian Koloko', 'Armoni Brooks', 'OG Anunoby', 'Scottie Barnes',
     'Precious Achiuwa', 'Justine Champagnie', 'David Johnson', 'Svi Mykhailiuk',
-    'Isaac Bonga', 'Yuta Watanabe', 'Thaddeus Young', 'Malachi Flynn', 'Fred VanVleet', 'Khem Birch', 'Chris Boucher', 'Gary Trent Jr.', 'Pascal Siakam', 'Dalano Banton']
+    'Isaac Bonga', 'Yuta Watanabe', 'Thaddeus Young', 'Malachi Flynn', 'Fred VanVleet',
+    'Khem Birch', 'Chris Boucher', 'Gary Trent Jr.', 'Pascal Siakam', 'Dalano Banton']
 
 # Function will choose one random
 # word from this list of words
-word = random.choice(words)
+  word = random.choice(words)
 
 
-print("Guess the characters")
+  print("Guess the characters")
 
-guesses = ''
+  guesses = ''
 
 # any number of turns can be used here
-turns = 12
+  turns = 12
 
 
-while turns > 0:
+  while turns > 0:
 
   # counts the number of times a user fails
-  failed = 0
+    failed = 0
 
   # all characters from the input
   # word taking one at a time.
-  for char in word:
+    for char in word:
 
     # comparing that character with
     # the character in guesses
-    if char in guesses:
-      print(char)
+      if char.casefold() in guesses:
+        print(char)
 
-    else:
-      print("_")
+      else:
+        print("_")
       #print(char, end=" ")
 
       # for every failure 1 will be
       # incremented in failure
       failed += 1
 
-  if failed == 0:
+    if failed == 0:
     # user will win the game if failure is 0
     # and 'You Win' will be given as output
-    print("You Win")
+      print("You Win")
 
     # this print the correct word
-    print("The word is: ", word)
-    break
+      print("The word is: ", word)
+      break
 
   # if user has input the wrong alphabet then
   # it will ask user to enter another alphabet
-  print()
-  guess = input("guess a character:")
+    print()
+    guess = input("guess a character:")
 
   # every input character will be stored in guesses
-  guesses += guess
+    guesses += guess
 
   # check input with the character in word
-  if guess not in word:
+    if guess not in word:
 
-    turns -= 1
+      turns -= 1
 
     # if the character doesn’t match the word
     # then “Wrong” will be given as output
-    print("Wrong")
+      print("Wrong")
 
     # this will print the number of
     # turns left for the user
-    print("You have", + turns, 'more guesses')
+      print("You have", + turns, 'more guesses')
 
-    if turns == 0:
-      print("You Loose")
+      if turns == 0:
+        print("You Loose")
+
+  user_continue = input("Would you like to play again? [y/n]? ")
+  if user_continue == ("y"):
+    continue
+  
+  elif user_continue == "n":
+      print("Good Bye!")
+      break
